a05ac0f016f9a389a4f9d7e6c434104da91d318b
its22013 <104542667+its22013@users.noreply.github.com>
2022-08-26 21:15:06 +0900

    Update block.py

--- block.py
+++ block.py
@@ -1,1 +1,13 @@
-main
+lass Block:
+    def __init__(self, canvas, color):
+
+        self.canvas = canvas
+      
+        self.id = self.canvas.create_rectangle(10, 10, 100, 50, fill=color)
+        
+
+    def delete(self):
+        self.canvas.delete(self.id, 0)
+
+        pos = self.canvas.coords(self.id)
+
